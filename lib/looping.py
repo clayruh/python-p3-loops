cbd19a08c665809884586d7023bd2b4b7b950611
Clae <56447997+clayruh@users.noreply.github.com>
2023-08-27 01:57:51 -0400

    solved

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
+# player_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+
+# inch_heights = print([height * 7920 for height in player_heights])
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+# happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_values = [abs(num * num) for num in int_list]
+    return squared_values
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if i%3 == 0 and i%5 == 0:
+            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+fizzbuzz()
